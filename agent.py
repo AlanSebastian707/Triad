08156603ea470d76c5a900aa920e830a7209c39a
ALAN SEBASTIAN <alnseb7077@gmail.com>
2026-08-08 11:48:34 +0530

    commit:3

--- agent.py
+++ agent.py
@@ -1,16 +1,31 @@
 import os
 import requests
 from dotenv import load_dotenv
+
 load_dotenv()
 API_KEY = os.environ["API_KEY"]
 API_URL = os.environ["API_BASE_URL"]
 MODEL = os.environ["MODEL"]
+
+SYSTEM_PROMPT = (
+    "You are a terminal coding assistant. always identify as the  a coding agent."
+)
+
+messages = [
+    {"role": "system", "content": SYSTEM_PROMPT},
+    {"role": "user", "content": "Who are you?"},
+]
+
 response = requests.post(
     API_URL,
     headers={"Authorization": f"Bearer {API_KEY}"},
     json={
         "model": MODEL,
-        "messages": [{"role": "user", "content": "Say hi in 5 words."}],
+        "messages": messages,
     },
 )
-print(response.json()["choices"][0]["message"]["content"])
+
+reply = response.json()["choices"][0]["message"]
+print(reply["content"])
+
+messages.append(reply)
